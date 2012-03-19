a708cf10c1230317ae8a04fd8b0b8a5afd1304f1
Ian Daniher <it.daniher@gmail.com>
2012-03-19 18:45:28 -0500

    powerSweep.py is now clean and calculates temperature.

--- tungsten/powerSweep.py
+++ tungsten/powerSweep.py
@@ -1,33 +1,47 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from pylab import linspace, subplot, plot, title, xlabel, ylabel, show, diff
-
+from pylab import *
 from connectClient import CEE
 CEE = CEE()
+ion()
 
-resistance = lambda vi: vi[0]/(vi[1]/1000)
-
-setAndGet = lambda i:  CEE.getInput('a', .1, 1, CEE.setOutput('a', 'i', i)['startSample']+100)
-
-spread = linspace(10, 130, 300)
-
-def WR(i):
-	v, i = setAndGet(i)
-	return v*i, resistance([v, i])
+setAndGet = lambda x:  CEE.getInput('a', .1, 1, CEE.setOutput('a', 'i', x)['startSample']+100)
 
 
-values = [WR(i) for i in spread] 
-values = zip(*values)
+voltages = []
+currents = []
 
-subplot(211)
-plot(values[0], values[1], '.')
-title("exploring the temperature-dependent resistance of a tungsten filament")
-ylabel(u"resistance (Ω)")
 
-subplot(212)
-plot(values[0][1::], diff(values[1]), '.')
+for mA in arange(5, 130):
+	[v, i] = setAndGet(mA)
+	voltages.append(v)
+	currents.append(i)
+
+voltage = array(voltages)
+current = array(currents)
+resistance = voltages/(current/1000)
+power = voltages*(current/1000)
+temperature = [((r/resistance[0])-1)/.004403+20 for r in resistance]
+
+subplot(221)
+plot(resistance, temperature, '.')
+xlabel(u"resistance (Ω)")
+ylabel(u"temperature (ºC)")
+
+subplot(223)
+semilogx(power[1::], diff(resistance), '.')
 ylabel(u"∂Ω/∂W")
-xlabel(u"power (mW)")
+xlabel("power (W)")
+
+subplot(222)
+loglog(power, temperature, '.')
+ylabel(u"temperature (ºC)")
+xlabel("power (W)")
+
+subplot(224)
+loglog(power[1::], diff(temperature), '.')
+ylabel(u"∂ºC/∂W")
+xlabel("power (W)")
 
 CEE.setOutput()
 show()
